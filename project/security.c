8c9401fddf58e9250c357d47d0afce00289bf279
Leon Liu <liuleon360@gmail.com>
2025-03-08 17:44:42 -0800

    Generating and sending client and server hello and finished

--- project/security.c
+++ project/security.c
@@ -6,26 +6,82 @@
 #include <string.h>
 #include <unistd.h>
 
+int __type; 
+char *__host;
+int phase = 0; // 0 = client hello, 1 = server hello, 2 = finished, 3 = data
+
+uint8_t *client_hello_data;
+uint8_t *server_hello_data;
+
+size_t client_hello_data_length;
+size_t server_hello_data_length;
+
 void init_sec(int type, char *host) {
     init_io();
+
+    __type = type;
+    __host = host;
+
+    phase = 0;
 }
 
-
 ssize_t input_sec(uint8_t *buf, size_t max_length) { 
-    ssize_t input_len = input_io(buf, max_length);
+    if (__type == CLIENT && phase == 0) {
+        // If client, and client hello
+        tlv* client_hello_tlv = generate_client_hello();
+        
+        // serialize to buffer
+        ssize_t client_hello_data_length = serialize_tlv(client_hello_data, client_hello_tlv);
 
-    return input_len;
+        free_tlv(client_hello_tlv);
+
+        memcpy(buf, client_hello_data, client_hello_data_length);
+
+        return client_hello_data_length;
+    }
+    else if (__type == SERVER && phase == 1) {
+        // If server, and server hello
+        tlv* server_hello_tlv = generate_server_hello();
+        
+        // serialize to buffer
+        ssize_t server_hello_data_length = serialize_tlv(server_hello_data, server_hello_tlv);
+
+        free_tlv(server_hello_tlv);
+
+        memcpy(buf, server_hello_data, server_hello_data_length);
+        
+        return server_hello_data_length;
+    }
+    else if (__type == CLIENT && phase == 2) {
+        // If client, and finished
+        tlv* finished_tlv = generate_finished(client_hello_data, client_hello_data_length, server_hello_data, server_hello_data_length);
+        
+        // serialize to buffer
+        ssize_t data_len = serialize_tlv(buf, finished_tlv);
+
+        free_tlv(finished_tlv);
+
+        return data_len;
+    }
+    else if (phase == 3) {
+        // If data read from stdin 
+        // Encrypt data
+
+        ssize_t input_len = input_io(buf, max_length);
+
+        return input_len;
+    }
+    
+    return 0;
 }
 
 void output_sec(uint8_t *buf, size_t length) { 
     // decrypt first
-    output_io(buf, length); 
+    output_io(buf, length);
 }
 
 // Generate a client hello TLV and return it
-// Returns serialized buffer on success, NULL on failure
-// *out_length will contain the total length of the resulting buffer
-tlv* generate_client_hello(size_t *out_length) {
+tlv* generate_client_hello() {
     // Create CLIENT_HELLO container
     tlv* client_hello_tlv = create_tlv(CLIENT_HELLO);
     
@@ -48,8 +104,7 @@ tlv* generate_client_hello(size_t *out_length) {
 }
 
 //  Generate a server hello tlv and return it
-// Returns NULL on error, caller must free the returned buffer
-tlv* generate_server_hello(size_t *out_length, const uint8_t *client_hello_data, size_t client_hello_length) {
+tlv* generate_server_hello(const uint8_t *client_hello_data, size_t client_hello_length) {
     tlv* server_hello_tlv = create_tlv(SERVER_HELLO);
 
     // Create and add NONCE
@@ -79,4 +134,23 @@ tlv* generate_server_hello(size_t *out_length, const uint8_t *client_hello_data,
     add_tlv(server_hello_tlv, signed_client_hello_tlv);
     
     return server_hello_tlv;
+}
+
+tlv* generated_finished(const uint8_t *client_hello_data, size_t client_hello_data, const uint8_t *server_hello_data, size_t server_hello_data) {
+    tlv* finished_tlv = create_tlv(FINISHED);
+
+    uint8_t client_server_hello_combined[client_hello_data_length + server_hello_data_length];
+    memcpy(client_server_hello_combined, client_hello_data, client_hello_data_length);
+    memcpy(client_server_hello_combined + client_hello_data_length, server_hello_data, server_hello_data_length);
+
+    uint8_t transcript[MAC_SIZE];
+
+    hmac(transcript, client_server_hello_combined, client_hello_data_length + server_hello_data_length);
+
+    tlv* transcript_tlv = create_tlv(TRANSCRIPT);
+    add_val(transcript_tlv, transcript, MAC_SIZE);
+
+    add_tlv(finished_tlv, transcript_tlv);
+
+    return finished_tlv;
 }
